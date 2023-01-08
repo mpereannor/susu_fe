92a563824b79181d69d234b9cf25b55ca122110a
mpere <mpereannor@gmail.com>
2023-01-08 21:21:06 +0000

    test current lint stage config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,17 @@
 const path = require('path')
 
+const buildTsCommand = (filenames) => `tsc`
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')}`
 
+const buildPrettierCommand = (filenames) =>
+  `prettier --write ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
+
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.{ts,tsx}': () => 'tsc --noEmit',
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand, buildPrettierCommand],
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
-
-  var a;
   return (
     <>
-<div><p>susu app test</p></div>
+      <div>
+        <p>susu app test</p>
+      </div>
     </>
   )
 }
