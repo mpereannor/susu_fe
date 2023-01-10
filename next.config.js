80ddbb01061c471e4136c3ad02d3f5d09d32d2ba
mpere <mpereannor@gmail.com>
2023-01-10 17:01:57 +0000

    chore: setup pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  swSrc: 'service-worker.js',
+})
+
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
