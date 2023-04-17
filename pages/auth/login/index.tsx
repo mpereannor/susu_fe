356337444baf1c297fbd4c44ac72ddfc192d3ca8
mpere <mpereannor@gmail.com>
2023-04-17 22:51:09 +0000

    feat: signin add password controls

--- pages/auth/login/index.tsx
+++ pages/auth/login/index.tsx
@@ -1,13 +1,15 @@
 // import { Stack, useColorModeValue } from "@chakra-ui/react"
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import {
+  Box,
   Button,
+  Center,
   Flex,
   FormControl,
   FormErrorMessage,
-  FormLabel,
   Heading,
   Input,
+  SimpleGrid,
   Stack,
   Text,
   useColorModeValue,
@@ -54,58 +56,135 @@ const Login = () => {
   return (
     <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
       <Flex flex={1} mx={{ base: 4, md: 0 }} bg={bg} color={color}>
-        <Stack spacing={4} w={'full'}>
-          <Heading textAlign={'center'} fontSize={20} fontWeight={'semibold'}>
-            Welcome back{' '}
-          </Heading>
-          <Text>Please log in to your account.</Text>
+        <Stack
+          spacing={8}
+          w={'full'}
+          height={'100%'}
+          align={'center'}
+          paddingTop={'15vh'}
+        >
+          <Box>
+            <Heading
+              textAlign={'center'}
+              fontSize={20}
+              fontWeight={'semibold'}
+              marginBottom={5}
+            >
+              Welcome back
+            </Heading>
+            <Text>Please log in to your account.</Text>
+          </Box>
           <form onSubmit={handleSubmit(onSubmit)}>
             <FormControl>
-              <Input
-                w={'300px'}
-                h={'60px'}
-                border={'1px'}
-                id="username"
-                placeholder="Enter your Username"
-                _focus={{
-                  borderColor: '#20ddbe',
-                }}
-                _placeholder={{
-                  color: 'gray.400',
-                }}
-                {...register('username', {
-                  required: 'Username is required',
-                })}
-              />
+              <Stack spacing={10}>
+                <Input
+                  w={'300px'}
+                  h={'60px'}
+                  border={'1px'}
+                  id="username"
+                  placeholder="Enter your Username"
+                  _focus={{
+                    borderColor: '#20ddbe',
+                  }}
+                  _placeholder={{
+                    color: 'gray.400',
+                  }}
+                  {...register('username', {
+                    required: 'Username is required',
+                  })}
+                />
 
-              <Input
-                w={'300px'}
-                h={'60px'}
-                id="pwd"
-                placeholder="Password"
-                _placeholder={{
-                  color: 'gray.400',
-                }}
-                _focus={{
-                  borderColor: '#20ddbe',
-                }}
-                {...register('pwd', {
-                  required: 'Password is required',
-                })}
-              />
+                {/* <Box> */}
+                <Input
+                  w={'300px'}
+                  h={'60px'}
+                  id="pwd"
+                  placeholder="Password"
+                  _placeholder={{
+                    color: 'gray.400',
+                  }}
+                  _focus={{
+                    borderColor: '#20ddbe',
+                  }}
+                  {...register('pwd', {
+                    required: 'Password is required',
+                  })}
+                />
+                <SimpleGrid columns={2} spacing={4}>
+                  <Center
+                    bg="rgba(32, 221, 190, 0.2)"
+                    height="40px"
+                    borderRadius={5}
+                  >
+                    <Text
+                      textAlign={'left'}
+                      fontSize={10}
+                      fontWeight={'medium'}
+                      color={'#718096'}
+                    >
+                      At least 8 characters
+                    </Text>
+                  </Center>
+                  <Center
+                    bg="rgba(32, 221, 190, 0.2)"
+                    height="40px"
+                    borderRadius={5}
+                  >
+                    <Text fontSize={10} fontWeight={'medium'} color={'#718096'}>
+                      At least one number
+                    </Text>
+                  </Center>
+                  <Center
+                    bg="rgba(32, 221, 190, 0.2)"
+                    height="40px"
+                    borderRadius={5}
+                  >
+                    <Text fontSize={10} fontWeight={'medium'} color={'#718096'}>
+                      At least one uppercase letter
+                    </Text>
+                  </Center>
+                  <Center
+                    bg="rgba(32, 221, 190, 0.2)"
+                    height="40px"
+                    borderRadius={5}
+                  >
+                    <Text fontSize={10} fontWeight={'medium'} color={'#718096'}>
+                      At least one special character
+                    </Text>
+                  </Center>
+                </SimpleGrid>
+                <FormErrorMessage>
+                  {errors.username && errors.username.message}
+                </FormErrorMessage>
+                {/* </Box> */}
 
-              <FormErrorMessage>
-                {errors.username && errors.username.message}
-              </FormErrorMessage>
-              <Button w={'300px'}
-                h={'60px'} color="white" _hover={{bg:"teal.400"}} bg='#20ddbe'>Continue</Button>
+                <Button
+                  w={'300px'}
+                  h={'60px'}
+                  color="white"
+                  _hover={{ bg: 'teal.400' }}
+                  bg="#20ddbe"
+                >
+                  Continue
+                </Button>
+              </Stack>
             </FormControl>
           </form>
-          <Text size="12px" fontWeight={500} color={"#20ddbe"}>New here? Create an account</Text>
+          <Text size="12px" fontWeight={500} color={'#20ddbe'}>
+            New here? Create an account
+          </Text>
         </Stack>
       </Flex>
       <Flex flex={1} pos={'relative'} display={{ base: 'none', md: 'block' }}>
-        <Image fill alt="susu_auth" src={susuAuthPic} object-fit="cover" />
+        <Image
+          style={{
+            width: '100%',
+            height: 'auto',
+          }}
+          alt="susu_auth"
+          src={susuAuthPic}
+          object-fit="cover"
+        />
       </Flex>
       {/* <ToggleColor /> */}
     </Stack>
