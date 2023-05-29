6189ac2c530d82737f073ec8fb42fda42d06ec7f
mpere <mpereannor@gmail.com>
2023-05-29 02:42:19 +0000

    feat: add extra sections to row items

--- templates/tables/index.tsx
+++ templates/tables/index.tsx
@@ -11,14 +11,34 @@ import {
   IconButton,
   HStack,
   Text,
+  VStack,
 } from '@chakra-ui/react'
-import { AiOutlineEdit } from 'react-icons/ai'
+import { Fragment, useCallback, useState } from 'react'
+import { AiOutlineArrowRight, AiOutlineEdit } from 'react-icons/ai'
 import { BsFillTrashFill } from 'react-icons/bs'
-import { IoIosArrowForward } from 'react-icons/io'
+import { IoIosArrowDown, IoIosArrowForward } from 'react-icons/io'
+import useToggle from '../../hooks/useToggle'
+import { ButtonData } from '../../types/dashboard'
+
+const defaultButtons: ButtonData[] = [
+  { id: 1, name: 'Baba Loki', balance: 'GHS 2,500,000' },
+  { id: 2, name: 'Marcus Gray', balance: 'GHS 800, 000' },
+  { id: 3, name: 'Selasi Gran', balance: 'GHS 6, 700, 00' },
+]
 
 const Tables = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [value, toggle] = useToggle()
+
+  const [toggledButtonId, setToggledButtonId] = useState<number | null>(null)
+  const toggleButton = useCallback((button: ButtonData) => {
+    setToggledButtonId((prevState) =>
+      prevState === button.id ? null : button.id
+    )
+  }, [])
+
   return (
-    <TableContainer>
+    <TableContainer maxWidth={'100%'}>
       <Table variant="simple" colorScheme="telegram">
         <Thead>
           <Tr>
@@ -28,117 +48,103 @@ const Tables = () => {
           </Tr>
         </Thead>
         <Tbody>
-          <Tr>
-            <Td
-              _hover={{
-                color: 'secondary.500',
-                cursor: 'pointer',
-              }}
-            >
-              <HStack>
-                <Text>Baba Loki</Text>
-                <IoIosArrowForward />
-              </HStack>
-            </Td>
-            <Td fontWeight={'semibold'}>GHS 2, 500, 000</Td>
-            <Td
-              _hover={{
-                color: 'secondary.500',
-                cursor: 'pointer',
-              }}
-            >
-              <Flex
-                justify={{
-                  md: 'end',
-                }}
-              >
-                <ButtonGroup variant="outline" size="sm" spacing={3}>
-                  <IconButton
-                    colorScheme="gray"
-                    icon={<AiOutlineEdit />}
-                    aria-label="Edit"
-                  />
-                  <IconButton
-                    colorScheme="gray"
-                    variant="outline"
-                    icon={<BsFillTrashFill />}
-                    aria-label="Delete"
-                  />
-                </ButtonGroup>
-              </Flex>
-            </Td>
-          </Tr>
-          <Tr>
-            <Td
-              _hover={{
-                color: 'secondary.500',
-                cursor: 'pointer',
-              }}
-            >
-              <HStack>
-                <Text>John Kwofie</Text>
-                <IoIosArrowForward />
-              </HStack>
-            </Td>
-            <Td fontWeight={'semibold'}>GHS 9,000,000</Td>
-            <Td>
-              <Flex
-                justify={{
-                  md: 'end',
-                }}
-              >
-                <ButtonGroup variant="outline" size="sm" spacing={3}>
-                  <IconButton
-                    colorScheme="gray"
-                    icon={<AiOutlineEdit />}
-                    aria-label="Edit"
-                  />
-                  <IconButton
-                    colorScheme="gray"
-                    variant="outline"
-                    icon={<BsFillTrashFill />}
-                    aria-label="Delete"
-                  />
-                </ButtonGroup>
-              </Flex>
-            </Td>
-          </Tr>
-          <Tr>
-            <Td
-              _hover={{
-                color: 'secondary.500',
-                cursor: 'pointer',
-              }}
-            >
-              {' '}
-              <HStack>
-                <Text>George Mensah</Text>
-                <IoIosArrowForward />
-              </HStack>
-            </Td>
-            <Td fontWeight={'semibold'}>GHS 8, 000, 000</Td>
-            <Td>
-              <Flex
-                justify={{
-                  md: 'end',
-                }}
-              >
-                <ButtonGroup variant="outline" size="sm" spacing={3}>
-                  <IconButton
-                    colorScheme="gray"
-                    icon={<AiOutlineEdit />}
-                    aria-label="Edit"
-                  />
-                  <IconButton
-                    colorScheme="gray"
-                    variant="outline"
-                    icon={<BsFillTrashFill />}
-                    aria-label="Delete"
-                  />
-                </ButtonGroup>
-              </Flex>
-            </Td>
-          </Tr>
+          {defaultButtons.map((button) => {
+            const isToggled = button.id === toggledButtonId
+            return (
+              <Fragment key={button.id}>
+                <Tr>
+                  <Td
+                    _hover={{
+                      color: 'secondary.500',
+                      cursor: 'pointer',
+                    }}
+                    onClick={() => toggleButton(button)}
+                  >
+                    {isToggled ? (
+                      <HStack onClick={toggle}>
+                        <Text>{button.name}</Text>
+                        <IoIosArrowDown />
+                      </HStack>
+                    ) : (
+                      <HStack onClick={toggle}>
+                        <Text>{button.name}</Text>
+                        <IoIosArrowForward />
+                      </HStack>
+                    )}
+                  </Td>{' '}
+                  <Td fontWeight={'semibold'}>{button.balance}</Td>
+                  <Td>
+                    <Flex
+                      justify={{
+                        md: 'end',
+                      }}
+                    >
+                      <ButtonGroup variant="outline" size="sm" spacing={3}>
+                        <IconButton
+                          colorScheme="gray"
+                          icon={<AiOutlineEdit />}
+                          aria-label="Edit"
+                        />
+                        <IconButton
+                          colorScheme="gray"
+                          variant="outline"
+                          icon={<BsFillTrashFill />}
+                          aria-label="Delete"
+                        />
+                      </ButtonGroup>
+                    </Flex>
+                  </Td>
+                </Tr>
+                <Tr
+                  bg={'secondary.50'}
+                  display={isToggled ? 'table-row' : 'none'}
+                >
+                  <Td>
+                    <VStack w={'100%'} gap={2} align={'flex-start'}>
+                      <HStack
+                        _hover={{
+                          color: 'secondary.500',
+                          cursor: 'pointer',
+                        }}
+                      >
+                        <Text>View Profile</Text>
+                        <AiOutlineArrowRight />
+                      </HStack>
+                      <HStack
+                        _hover={{
+                          color: 'secondary.500',
+                          cursor: 'pointer',
+                        }}
+                      >
+                        <Text>View Deposits</Text>
+                        <AiOutlineArrowRight />
+                      </HStack>
+                      <HStack
+                        _hover={{
+                          color: 'secondary.500',
+                          cursor: 'pointer',
+                        }}
+                      >
+                        <Text>View Withdrawals</Text>
+                        <AiOutlineArrowRight />
+                      </HStack>
+                      <HStack
+                        _hover={{
+                          color: 'secondary.500',
+                          cursor: 'pointer',
+                        }}
+                      >
+                        <Text>View Savings</Text>
+                        <AiOutlineArrowRight />
+                      </HStack>
+                    </VStack>
+                  </Td>
+                  <Td></Td>
+                  <Td></Td>
+                </Tr>
+              </Fragment>
+            )
+          })}
         </Tbody>
       </Table>
     </TableContainer>
---
+++ types/dashboard.ts
@@ -0,0 +1,5 @@
+export interface ButtonData {
+  id: number,
+  name: string,
+  balance: string
+}
